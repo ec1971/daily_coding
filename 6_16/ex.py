f2f7788a40ffa036990a31f639c456687feccac4
Jingchao Liu <liujingchao0519@gmail.com>
2020-06-16 21:05:44 -0400

    finish challenge 5

--- 6_16/ex.py
+++ 6_16/ex.py
@@ -1,4 +1,6 @@
 import re
+import urllib.request
+import pickle
 
 p = re.compile("[^A-Z][A-Z]{3}([a-z]){1}[A-Z]{3}[^A-Z]")
 def matched(text):
@@ -13,7 +15,39 @@ def solve():
                 count += 1
                 print(matched(l))
     print(count)
+
+def getContent(current):
+
+    return content
+
+
+def test():
+    url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=8022"
+#    url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+    address = url.split('=')[0]
+    with open("result.txt", 'w') as f:
+        for i in range(10000):
+            response = urllib.request.urlopen(url)
+            content = response.read().decode("utf-8")
+            f.write(content)
+            f.write("\n")
+            url = address + "=" + content.split()[-1]
             
 
+
+def peakHell():            
+    url = "http://www.pythonchallenge.com/pc/def/banner.p"
+    response = urllib.request.urlopen(url)
+    content = response.read()
+    content = pickle.loads(content)
+    for line in content:
+        output(line)
+
+def output(text):
+    cur = ""
+    for item in text:
+        cur += item[1] * item[0]
+    print(cur)
+
 if __name__=="__main__":
-    solve()
+    peakHell()
