41bb2ebeb0e8cc59290d9a67f1844e56aa683f36
Jingchao Liu <liujingchao0519@gmail.com>
2020-07-21 23:54:10 -0400

    add ex30

--- SICP/ex29.scm
+++ SICP/ex29.scm
@@ -1,5 +1,5 @@
+
 (define (simpson f a b n)
-    (define (even? x) (= (remainder x 2) 0))
     (define h (/ (- b a) n))
     (define (factor k)
         (cond ((or (= 0 k)(= n k)) 1)
@@ -9,12 +9,18 @@
         (* (factor k)(f (+ a (* k h)))))
     (*(/ h 3) (sum term 0 inc n)))
 
+;utils
+(define (even? x) (= (remainder x 2) 0))
 (define (cube x) (* x x x))
 (define (inc x) (+ x 1))
+
+;sum
 (define (sum term a next b) 
    (if (> a b) 
        0 
        (+ (term a) 
           (sum term (next a) next b)))) 
+;test
+
 (simpson cube 0 1.0 100)
 (simpson cube 0 1.0 1000)
