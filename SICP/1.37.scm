bbf35b4e30add0c10dbf6a0d5e9c2d232e319ef8
Jingchao Liu <liujingchao0519@gmail.com>
2020-07-25 15:46:52 -0400

    add 37

---
+++ SICP/1.37.scm
@@ -0,0 +1,23 @@
+;recursive
+(define (cont-frac n d k)
+    (define (compute i) (if (= i k) 
+            (/ (n i)(d i))
+            (/ (n i)(+ (d i)(compute (+ i 1))))))
+    (compute 1))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+    11)
+
+;iter
+(define (cont-frac n d k)
+    (define (iter i result) 
+        (if (= i 0) 
+            result
+            (iter (- i 1) (/ (n i)
+                             (+ result (d i))))))
+    (iter k 0))
+
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+    11)
