72400df967a88c569f149102d89399fa8cc2cfff
Jingchao Liu <liujingchao0519@gmail.com>
2020-07-26 15:50:25 -0400

    modify 1.43

--- SICP/1.43.scm
+++ SICP/1.43.scm
@@ -1,8 +1,15 @@
 (load "util")
+;original
 (define (repeated f n) 
     (lambda (x)
     (cond ((= n 1)(f x))
           ((even? n)((repeated (compose f f) (/ n 2)) x))
           (else ((repeated f (- n 1))(f x)))))) 
+
+;cleaned up version
+(define (repeated f n) 
+   (cond ((= n 0) identity) 
+         ((even? n) (repeated (compose f f) (/ n 2))) 
+         (else (compose f (repeated f (- n 1)))))) 
 ((repeated square 2) 5)
         
