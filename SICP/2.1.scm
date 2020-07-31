f1c8ed19ae30d3d86cd686ce5ae30667952c17e1
Jingchao Liu <liujingchao0519@gmail.com>
2020-07-30 21:13:44 -0400

    fix gcd

--- SICP/2.1.scm
+++ SICP/2.1.scm
@@ -1,6 +1,7 @@
 (load "util")
 
 ;implementation
+;need to fix the sign otherwise gcd would fail
 (define (make-rat n d)
   (let ((g ((if (< d 0) - +) (abs (gcd n d)))))
     (cons (/ n g) (/ d g))))  
@@ -18,6 +19,6 @@
                  (* (denom x)(numer y)))
               (* (denom x)(denom y))))
 
-
-(add (make-rat -1 3) (make-rat 2 3))
+(trace make-rat)
+(add (make-rat (- 1) 3) (make-rat 2 3))
     
