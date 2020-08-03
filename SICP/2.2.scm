7af8a273912a028ea9ea2ba680ee1d40d6b2db46
Jingchao Liu <liujingchao0519@gmail.com>
2020-08-03 08:42:50 -0400

    finish 2.2

--- SICP/2.2.scm
+++ SICP/2.2.scm
@@ -9,19 +9,28 @@
 
 ; segment implementation
 ;takes in two points
-(define (make-segment p1 p2)())
-(define (start-segment seg)())
-(define (end-segment seg)())
+(define (make-segment p1 p2)
+    (cons p1 p2))
+(define (start-segment seg)(car seg))
+(define (end-segment seg)(cdr seg))
 
 ; point implementation
-(define (make-point x y)())
-(define (x-point p)())
-(define (y-point p)())
+(define (make-point x y)
+    (cons x y ))
+(define (x-point p)(car p))
+(define (y-point p)(cdr p))
 
 ; takes in segment and return midpoint
-(define (midpoint-segment seg)())
+(define (midpoint-segment seg)
+    (define (mid-pos x y)
+        (/ (+ x y) 2))
+    (make-point 
+        (mid-pos (car (start-segment seg))
+                 (car (end-segment seg)))
+        (mid-pos (cdr (start-segment seg))
+                 (cdr (end-segment seg)))))
 
 (print-point 
     (midpoint-segment 
-        (make-segment (make-point -1 5)
-                      (make-point 2 3))))
+        (make-segment (make-point -1.0 5.0)
+                      (make-point 2.0 3.0))))
