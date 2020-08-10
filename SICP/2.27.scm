7fa5d82d705de35bb41364d6b109c05e0aa729c1
Jingchao Liu <liujingchao0519@gmail.com>
2020-08-10 09:55:38 -0400

    modified 27 to make it correct

--- SICP/2.27.scm
+++ SICP/2.27.scm
@@ -7,6 +7,10 @@
 (define (deep-reverse lst)
     (if (null? lst) 
         lst 
-        (append (reverse (cdr lst))(list (reverse (car lst))))))
+        (if (pair? (car lst))
+            (append (deep-reverse (cdr lst))(list (deep-reverse (car lst))))
+            (append (deep-reverse (cdr lst))(list (car lst))))))
 
-(deep-reverse (list (list 1 2) 3 4))
+  
+
+(deep-reverse (list (list 1 2) (list 3 4 (list 5 6) )))
